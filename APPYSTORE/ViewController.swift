d5dccddfc6fda79036f7e2dd7bc95443cf569a87
rahulmandalkar <rahulmandalkar431@gmail.com>
2017-02-22 16:48:22 +0530

    Initial Commit

---
+++ APPYSTORE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  APPYSTORE
+//
+//  Created by BridgeLabz Solution LLP on 22/02/17.
+//  Copyright © 2017 BridgeLabz Solution LLP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
